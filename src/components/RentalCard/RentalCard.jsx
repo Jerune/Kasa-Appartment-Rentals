e8178084533e5a362e36867bc7890dfa1a8c7a5d
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-06-13 21:54:32 +0200

    feat: Adding RentalCards
    Add RentalCards components to display on homepage with dummy content

---
+++ src/components/RentalCard/RentalCard.jsx
@@ -0,0 +1,12 @@
+const backgroundStyles = {
+    background:
+          "center cover no-repeat url(https://i.picsum.photos/id/219/200/300.jpg?hmac=RGnJfbO2380zLCFSj2tm_q0vW0wtw67d0fhWHX2IoDk)"
+}
+
+export default function RentalCard(){
+    return(
+        <div className="rental-card" style={backgroundStyles}>
+            <h2>test</h2>
+        </div>
+    )
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,19 @@
+import RentalCard from "../../components/RentalCard/RentalCard";
+
 export default function Home(){
     return(
         <main className="main">
             <header className="hero-banner">
                 <h1>Chez vous, partout et ailleurs</h1>
             </header>
+            <section className="rental-overview">
+                <RentalCard />
+                <RentalCard />
+                <RentalCard />
+                <RentalCard />
+                <RentalCard />
+                <RentalCard />
+            </section>
         </main>
     )
 }

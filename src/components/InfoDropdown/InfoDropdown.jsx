31b2f3fbd390b08c5160124909832230c7159617
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-06-14 10:27:09 +0200

    feat: Adding State and Events to InfoDropdown
    InfoDropdown component now shows or hides paragraph content based on active state when click event is triggered

--- src/components/InfoDropdown/InfoDropdown.jsx
+++ src/components/InfoDropdown/InfoDropdown.jsx
@@ -1,11 +1,23 @@
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleUp, faAngleDown } from '@fortawesome/free-solid-svg-icons'
+
 export default function InfoDropdown(){
+    const [dropdownState, setDropdownState] = useState('closed');
+
+    function ToggleDropdown(state){
+        state === 'open' ? setDropdownState('closed') : setDropdownState('open');
+        console.log(dropdownState);
+    }
+
     return(
         <article className="info-dropdown">
-            <div>
+            <div onClick={() => ToggleDropdown(dropdownState)}>
                 <h2>Awesome Title</h2>
-                <i>icon</i>
+                {dropdownState==='closed' ? <FontAwesomeIcon icon={faAngleDown} /> :
+                <FontAwesomeIcon icon={faAngleUp} />}
             </div>
-            <p>This is a lot of dummy text to see if we can render this explanatory text in a nice and beautiful way so we know if we can afterwards transform it to real data.</p>
+            {dropdownState==='open' ? <p>This is a lot of dummy text to see if we can render this explanatory text in a nice and beautiful way so we know if we can afterwards transform it to real data.</p> : null}
         </article>
     )
 }
